e39f8eaa268d248194a7964b55c022fe72e98122
Stoyan Bochev <147409926+StoyanBochev@users.noreply.github.com>
2023-10-09 22:53:02 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,17 @@
+number1 = float(input("Enter first number: "))
+number2 = float(input("Enter second number: "))
+action = input("*, /, +, -: ")
+
+if action == "*":
+    print(number1 * number2)
+
+elif action == "/":
+    print(number1 / number2)
+
+elif action == "+":
+    print(number1 + number2)
+
+elif action == "-":
+    print(number1 - number2)
+
+
